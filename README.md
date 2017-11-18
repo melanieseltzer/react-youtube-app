a01949ad0bbba13058adcfe7f8b9020572e689e7
Melanie Seltzer <melleh11@gmail.com>
2017-11-17 16:15:46 -0800

    Finishing off section 2 of course - full working app

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # React Youtube App
 
 Thank you very much! https://www.udemy.com/react-redux/
+
+# Local Development
+
+```
+npm install
+```
+
+```
+npm start
+```
+
+View locally: http://localhost:8080/
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -20,12 +20,17 @@ class SearchBar extends Component {
     // Pass the event handler to the element for monitoring
     // Always manipulate state with this.setState
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
